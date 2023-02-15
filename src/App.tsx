415a10cc0fb9a09da9dcd6b724e1b6426c455d5e
JulianaVelasques <juliabalta16@gmail.com>
2023-02-14 22:32:50 -0300

    fix: remove not used files and add the home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { useState } from "react";
-
-import "./App.css";
+import { Home } from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div>
+      <Home />
     </div>
   );
 }
