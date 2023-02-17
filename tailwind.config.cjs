0d625ee9fb661f6d9406891557b3502069a7537a
JulianaVelasques <juliabalta16@gmail.com>
2023-02-16 22:49:53 -0300

    feat: add font and config colors variables on tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,css}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-mode': '#F6F8FF',
+        'dark-mode': '#141D2F',
+        'primary-font-dark': '#222731',
+        'secondary-font-grey': '#4B6A9B',
+        'tertiary-blue': '#0079FF',
+      },
+      fontFamily: {
+        'space-mono': ['Space Mono', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
